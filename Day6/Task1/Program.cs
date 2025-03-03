bd3a41e4634de83f7e4b4bcf8c37b5fa3fb7afe9
Pranit0720 <patilpranit580@gmail.com>
2025-03-03 10:03:50 +0530

    commit
    day 6 Assignment Done

---
+++ Day6/Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿using Task1.Model;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank bank = new Bank();
+            Customer customer1 = new Customer(1,"Pranit");
+            customer1.GetTokenNumber(bank);
+            Customer customer2 = new Customer(2, "Kapil");
+            customer2.GetTokenNumber(bank);
+            Customer customer3 = new Customer(3, "Aatharva");
+            customer3.GetTokenNumber(bank);
+
+            bank.NextInQueue();
+            bank.OutFromQueue();
+            bank.NextInQueue();
+        }
+    }
+}
---
+++ Day6/Task2/Exception/IdAlreadyRegistedException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2.Exception
+{
+    internal class IdAlreadyRegistedException:ApplicationException
+    {
+        public IdAlreadyRegistedException()
+        {
+            
+        }
+        public IdAlreadyRegistedException(string message):base(message)
+        {
+        }
+    }
+}
