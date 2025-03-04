96c502ec0c756162ed619dc721391cd01660a310
Pranit0720 <patilpranit580@gmail.com>
2025-03-04 09:50:23 +0530

    commit

---
+++ Day7/Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿using Task1.Model;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee(13554,"Pranit Patil",23, new DateTime(2018,01,19));
+            employee.calculateExperience();
+
+
+
+        }
+        }
+}
