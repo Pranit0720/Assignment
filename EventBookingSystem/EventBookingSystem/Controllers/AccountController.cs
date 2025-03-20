998a2975c23d53e0d664b361eb5151fba6392415
Pranit0720 <patilpranit580@gmail.com>
2025-03-20 22:30:43 +0530

    commit

---
+++ EventBookingSystem/EventBookingSystem/Controllers/AccountController.cs
@@ -0,0 +1,41 @@
+﻿using EventBookingSystem.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventBookingSystem.Controllers
+{
+    public class AccountController : Controller
+    {
+        readonly UserManager<IdentityUser> _userManager;
+        readonly SignInManager<IdentityUser> _signInManager;
+        public AccountController(UserManager<IdentityUser>manager,SignInManager<IdentityUser>signInManager)
+        {
+            _userManager = manager;
+            _signInManager = signInManager;
+            
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel user)
+        {
+            if (ModelState.IsValid)
+            {
+                var createdUser = new IdentityUser { UserName = user.Email, Email = user.Email };
+                var result = await _userManager.CreateAsync(createdUser, user.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("GetAllEvents","Event");
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+            return View(user);
+        }
+    }
+}
